6e3408bb588fe068b53450bff1fedbc8d94add96
jjinheeWorld <wlsgml450@naver.com>
2022-11-22 05:04:32 +0900

    마크업에서 Vue 인스턴스 사용하기

---
+++ 02_vue_basic/first_vue1/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>저는 {{age}}살입니다.</h2>
+    <button v-on:click="increase">증가</button>
+    <button v-on:click="decrease">감소</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return { age: 27}
+  },
+  methods: {
+    increase() {
+      this.age++;
+    },
+    decrease() {
+      this.age--;
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
