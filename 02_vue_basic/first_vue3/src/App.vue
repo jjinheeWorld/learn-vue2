31ddee6e0e1bb5538d0ced3e7ef905b3ff65de4c
jjinheeWorld <wlsgml450@naver.com>
2022-11-25 15:52:02 +0900

    Vue 클래스 바인딩 (배열)

---
+++ 02_vue_basic/first_vue3/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <!-- 배열 -->
+    <div v-bind:class="[active, error]">코코몽은</div>
+    <div v-bind:class="[{active: isActive}, error]">정말</div>
+    <!-- 삼항 연산자 -->
+    <div v-bind:class="[isActive ? active : text]">귀여워</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+       isActive: true,
+       active: 'active',
+       error: 'text'
+    }
+  },
+  computed: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.active {border: 1px solid red}
+</style>
