599f22897af619603527a3514ad578539d8e43ee
jjinheeWorld <wlsgml450@naver.com>
2022-11-27 02:42:48 +0900

    Vue click 토글 버튼

---
+++ 02_vue_basic/first_vue8/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div v-if="hi">안녕하세요~</div>
+    <div v-else>안녕히가세요!</div>
+    <button @click="change">바꾸기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      hi: true,
+    }
+  },
+  methods: {
+  change() {
+    this.hi = !this.hi;
+   }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
