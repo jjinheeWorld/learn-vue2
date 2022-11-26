33224c4cb579684d1c131471808d17e390470edb
jjinheeWorld <wlsgml450@naver.com>
2022-11-26 17:35:29 +0900

    Vue 스타일 바인딩

---
+++ 02_vue_basic/first_vue4/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <div v-bind:style="{color:activeColor, fontSize: fontSize + 'px'}">Hi</div> -->
+    
+    <div v-bind:style="activeStyle">Hi</div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      activeStyle: {
+      color: 'red',
+      fontSize: '30px'
+      },
+      activeColor: 'red',
+      fontSize: 32,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
