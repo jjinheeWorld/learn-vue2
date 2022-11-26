e8e26840b80649d5096e9d8319081cafadf14fa6
jjinheeWorld <wlsgml450@naver.com>
2022-11-27 01:16:00 +0900

    Vue 이벤트 핸들링

---
+++ 02_vue_basic/first_vue5/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <!-- 이벤트 객체 -->
+   <!-- <button v-on:click="sumAge">add</button> -->
+
+   <!-- 파라미터 전달-->
+   <button v-on:click="sumAge(27)">add</button>
+   <p>{{counter}}</p>
+
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+  methods: {
+    // sumAge(e) { // 이벤트 객체
+    //   this.counter++;
+    //   console.log(e.target);
+    // }
+    sumAge(num) { // 이벤트 객체 ❌
+      this.counter += num;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02_vue_basic/first_vue6/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+   <ul>
+    <li v-for="(item, i) in arr" v-bind:key="i">
+      이름은 {{item.name}}이고, 나이는 {{item.age}}살이다.
+      <button v-on:click="sumAge(item.age, $event)">add</button></li>
+   </ul>
+   
+   <p>{{counter}}</p>
+
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter: 0,
+      arr: [
+        {name: 'a', age: 27},
+        {name: 'b', age: 28},
+        {name: 'c', age: 29},
+      ],
+    }
+  },
+  methods: {
+    sumAge(num, e) { // 파라미터와 이벤트객체
+      this.counter += num;
+      e.target.parentNode.style = 'border: 1px solid red'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
