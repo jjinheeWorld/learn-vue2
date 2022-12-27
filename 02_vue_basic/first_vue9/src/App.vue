cb41987bfe48b04dd1754d5e3a569dbdd6b05506
jjinheeWorld <wlsgml450@naver.com>
2022-12-27 21:33:19 +0900

    fetch함수로 전달 받은 데이터로 Vue 실습 (Users)

---
+++ 02_vue_basic/first_vue9/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <h2>Students List</h2>
+    <ul class="list">
+      <li v-for="user of users" :key="user.name">
+        <div><strong>Name</strong> : {{ user.name }} <span>({{ user.username }})</span></div>
+        <div><strong>City</strong> : {{ user.address.city }}, {{ user.address.zipcode }} <span>({{ user.address.geo.lat }}, {{ user.address.geo.lng }})</span></div>
+        <div><strong>Street</strong> : {{ user.address.street }} <span>({{ user.address.zipcode }})</span></div>
+        <div><strong>E-mail</strong> : <a :href="'mailto:' + user.email">{{ user.email }}</a></div>
+        <div><strong>Phone</strong> : {{ user.phone }} </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: '',
+    }
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+        .then(users => {this.users = users});
+   }
+}
+</script>
+
+<style>
+* {margin: 0; padding: 0;}
+body {padding: 30px;}
+li {list-style: none;}
+
+h2 {
+  color: rgb(5, 65, 168);
+  font-size: 32px;
+}
+.list > li {
+  background-color: #e4e4e4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 20px 0;
+}
+.list > li > div {
+  margin: 8px 0;
+}
+.list > li strong {
+  display: inline-block;
+  width: 50px;
+}
+.list > li  span {
+  font-size: 12px;
+  color: #999;
+}
+</style>
